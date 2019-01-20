10c644814b22221daa6270edb94ce71c0c4ef712
Miles <Miles@Lyns-MacBook-Air.local>
2019-01-20 15:49:42 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,40 @@
 
 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+}
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+}
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+}
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png",
+}
+];
+
+
 var cardsInPlay = [];
 var checkForMatch = function (){};
 var flipCard = function(cardId) {
 	flipCard(0);
 	flipCard(2);
-console.log("User flipped " + cards[cardId]); 
-cardsInPlay.push(cards[cardId]);
-	
+console.log("User flipped " + cards[cardId].rank); 
+cardsInPlay.push(cards[cardId].rank);
 
-
-
+console.log("User flipped " + cardImage);
+console.log("User flipped " + suit);
 
 
 if (cardsInPlay.length === 2) 
@@ -25,7 +47,11 @@ if (cardsInPlay[0] === cardsInPlay[1]) {
 
 }; // flipcard function ends here
 
-
+var createBoard = function ();
+for (var i = 0; i <cardsInPlay.length; i++){
+	cardElement.setAttribute('backOfCard','images/back.png');
+	cardElement.setAttribute('data-id', i)
+}
 
 
 
