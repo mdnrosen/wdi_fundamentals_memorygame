ec9d5e3dd23e3c6ed4445ee443c57ffe73f1c8fb
Miles <Miles@Lyns-MacBook-Air.local>
2019-01-20 14:39:23 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,27 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("user flipped " + cardOne);
-console.log("user flipped " + cardThree);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push('cardOne'); {
+	console.log("user flipped " + cardOne);	
+};
+
+cardsInPlay.push('cardTwo'); {
+	console.log("user flipped " + cardTwo);
+};
+
+if (cardsInPlay.length === 2) 
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match");
+} else {
+	alert("Bad luck, try again.");
+}
+	
 
 
 
