8bfd7f0b5ff05d80b008ded8a99770b7819a8bc9
Miles <Miles@Lyns-MacBook-Air.local>
2019-01-20 13:58:50 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,11 @@
-console.log("Up and running!");
-
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("user flipped " + cardOne);
+console.log("user flipped " + cardThree);
+
+
 
 
