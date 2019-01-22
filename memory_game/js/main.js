c0bc6228b953dd18f897039042c72e56d4914b8c
Miles <Miles@Lyns-MacBook-Air.local>
2019-01-22 12:48:57 +0000

    final

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,22 +6,22 @@ var cards = [
 	rank: "queen",
 	suit: "hearts",
 	cardImage: "images/queen-of-hearts.png"
-}
+},
 {
 	rank: "queen",
 	suit: "diamonds",
 	cardImage: "images/queen-of-diamonds.png",
-}
+},
 {
 	rank: "king",
 	suit: "hearts",
 	cardImage: "images/king-of-hearts.png",
-}
+},
 {
 	rank: "king",
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png",
-}
+},
 ];
 
 
@@ -47,7 +47,7 @@ console.log(cards[cardId].cardImage);
 console.log(cards[cardId].suit);
 };
 
-var createBoard = function ();
+var createBoard = function () {
 for (var i = 0; i <cards.length; i++){
 	var cardElement = document.createElement('img');
 	cardElement.setAttribute('src','images/back.png');
@@ -55,7 +55,7 @@ for (var i = 0; i <cards.length; i++){
 	cardElement.addEventListener('click',flipCard);
 	document.getElementById('game-board').appendChild(cardElement);
 }
-;
+};
 
 createBoard();
 
